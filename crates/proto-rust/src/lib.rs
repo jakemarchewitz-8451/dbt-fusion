b88ecdff6cc6bb255beac6ff1ad669ce09a98c89
Mike Perlov <5206955+mishamsk@users.noreply.github.com>
2025-09-16 17:16:50 -0400

    Workaround opentelemetry upstream breaking version change (#5773)
    * fix conflicting prost versions
    
    * temporarily force lock of otel proto to compatible version
    
    * fix hakari
    
    GitOrigin-RevId: f204a77e71b40c2592c0e0832c7f05a811202f29

--- crates/proto-rust/src/lib.rs
+++ crates/proto-rust/src/lib.rs
@@ -23,33 +23,3 @@ pub mod v1 {
         }
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use prost::Name;
-
-    // We only enable reflection for the public protobuf events.
-    static PUBLIC_DESCRIPTOR_POOL: &[u8] = include_bytes!("gen/dbtlabs_proto.bin");
-
-    #[test]
-    /// Test for presence of adapter events
-    fn test_adapter_events() {
-        let pool = prost_reflect::DescriptorPool::decode(PUBLIC_DESCRIPTOR_POOL).unwrap();
-
-        let file_descriptor = pool.get_message_by_name(
-            crate::v1::public::fields::adapter_types::SqlQueryStatus::type_url()
-                .split("/")
-                .last()
-                .unwrap(),
-        );
-
-        println!("file_descriptor = {file_descriptor:?}");
-
-        if let Some(file_descriptor) = file_descriptor {
-            let extensions = file_descriptor.extensions();
-            for extension in extensions {
-                println!("extension = {extension:?}");
-            }
-        }
-    }
-}
