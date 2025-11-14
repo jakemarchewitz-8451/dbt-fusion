f832b697c2e78cafd5748ff3a943aa9aa56268f8
jakemarchewitz-8451 <jake.marchewitz@8451.com>
2025-11-14 14:05:12 -0600

    commit with signing key

---
+++ crates/dbt-py/src/lib.rs
@@ -0,0 +1,131 @@
+use std::process::ExitCode;
+
+use clap::Parser;
+use clap::error::ErrorKind;
+use dbt_common::cancellation::CancellationTokenSource;
+use dbt_sa_lib::dbt_sa_clap::Cli;
+use dbt_sa_lib::dbt_sa_lib::{print_trimmed_error, run_with_args};
+use pyo3::exceptions::{PySystemExit, PyTypeError, PyValueError};
+use pyo3::prelude::*;
+use pyo3::types::{PyList, PyString};
+
+// JAKE: `from dbt_fusion import dbtRunner; dbtRunner().invoke(["deps", "--project-dir", "/Users/j293015/Repos/insightsos-dbt"])`
+#[allow(non_camel_case_types, unused)]
+#[pyclass(unsendable)]
+struct dbtResult {
+    success: bool,
+    exception: Option<PyErr>,
+    result: Option<PyObject>,
+}
+
+impl dbtResult {
+    fn from_py_err(err: PyErr) -> Self {
+        dbtResult {
+            success: false,
+            exception: Some(err),
+            result: None,
+        }
+    }
+}
+
+#[allow(non_camel_case_types)]
+#[pyclass(unsendable)]
+struct dbtRunner;
+
+#[pymethods]
+impl dbtRunner {
+    #[new]
+    fn new() -> Self {
+        dbtRunner {}
+    }
+
+    /// Invoke dbt
+    fn invoke(&self, args: &Bound<'_, PyList>) -> dbtResult {
+        let mut str_args: Vec<String> = Vec::new();
+
+        // We are responsible for adding the dbt command at the beginning to make parsing work correctly
+        str_args.push("dbt".to_owned());
+
+        for (idx, item) in args.iter().enumerate() {
+            // Validate all items passed into list are strings
+            if !item.is_instance_of::<PyString>() {
+                let err_str = format!(
+                    "invoke() must recieve a list of strings. The object at position {} in the list is not a string.",
+                    idx
+                );
+
+                let res = dbtResult::from_py_err(PyTypeError::new_err(err_str));
+
+                return res;
+            }
+
+            // Extract each item into a Rust String
+            let s = match item.extract::<String>() {
+                Ok(s) => s,
+                Err(e) => {
+                    let res = dbtResult {
+                        success: false,
+                        exception: Some(e),
+                        result: None,
+                    };
+
+                    return res;
+                }
+            };
+
+            // Prevent users from adding the dbt command itself, we handle that
+            if idx == 0 && s.trim() == "dbt" {
+                let err_str = "invoke() should only recieve subcommands and arguments for the dbt CLI command, don't pass in `dbt` at the beginning - e.g. [\"init\"], not [\"dbt\", \"init\"]";
+
+                let res = dbtResult::from_py_err(PyValueError::new_err(err_str));
+
+                return res;
+            }
+
+            str_args.push(s);
+        }
+
+        let cst = CancellationTokenSource::new();
+        let token = cst.token();
+
+        let cli = match Cli::try_parse_from(str_args.iter()) {
+            Ok(cli) => cli,
+            Err(e) => {
+                if e.kind() == ErrorKind::UnknownArgument {
+                    // todo make this for more than just unknown arguments
+                    // Only show the actual error message
+                    let msg = e.to_string(); // includes both "error:" and possibly "tip:"
+                    print_trimmed_error(msg); // prints to stderr
+                    std::process::exit(1);
+                } else {
+                    // For other errors, show full help as usual
+                    e.exit();
+                }
+            }
+        };
+
+        let status_code = run_with_args(cli, token);
+
+        if status_code != ExitCode::SUCCESS {
+            let res = dbtResult::from_py_err(PySystemExit::new_err(format!(
+                "dbt exited with code {:?}.",
+                status_code
+            )));
+
+            return res;
+        }
+
+        dbtResult {
+            success: true,
+            exception: None,
+            result: None,
+        }
+    }
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn dbt_fusion(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<dbtRunner>()?;
+    Ok(())
+}
--- crates/dbt-sa-cli/src/dbt_sa_lib.rs
+++ crates/dbt-sa-cli/src/dbt_sa_lib.rs
@@ -1,10 +1,14 @@
-use crate::dbt_sa_clap::{Cli, Commands, ProjectTemplate};
+use crate::dbt_sa_clap::{Cli, Commands, ProjectTemplate, from_main};
 use dbt_common::cancellation::CancellationToken;
 use dbt_common::create_root_info_span;
 use dbt_common::io_utils::checkpoint_maybe_exit;
-use dbt_common::tracing::emit::{emit_error_log_from_fs_error, emit_info_log_message};
-use dbt_common::tracing::invocation::create_invocation_attributes;
-use dbt_common::tracing::metrics::get_exit_code_from_error_counter;
+use dbt_common::tracing::{
+    emit::{emit_error_log_from_fs_error, emit_info_log_message},
+    invocation::create_invocation_attributes,
+    metrics::get_exit_code_from_error_counter,
+    FsTraceConfig,
+    init_tracing
+};
 use dbt_init::init;
 use dbt_jinja_utils::invocation_args::InvocationArgs;
 use dbt_jinja_utils::listener::DefaultJinjaTypeCheckEventListenerFactory;
@@ -14,11 +18,12 @@ use dbt_schemas::man::execute_man_command;
 use dbt_common::io_args::{EvalArgs, EvalArgsBuilder};
 use dbt_common::{
     ErrorCode, FsResult,
-    constants::{DBT_MANIFEST_JSON, INSTALLING, VALIDATING},
+    constants::{DBT_MANIFEST_JSON, INSTALLING, ERROR, PANIC, VALIDATING},
     fs_err, fsinfo,
     io_args::{Phases, SystemArgs},
     logging::init_logger,
-    pretty_string::GREEN,
+    pretty_string::{GREEN, RED},
+
     show_progress, show_result_with_default_title, stdfs,
     tracing::span_info::record_span_status,
 };
@@ -33,12 +38,27 @@ use git_version::git_version;
 use dbt_schemas::schemas::manifest::build_manifest;
 use tracing::Instrument;
 
+use std::process::ExitCode;
 use std::sync::Arc;
 
 use dbt_loader::{args::LoadArgs, load};
 use dbt_parser::{args::ResolveArgs, resolver::resolve};
 
 use serde_json::to_string_pretty;
+
+
+// JAKE:
+use std::io::{self, Write};
+
+// Vars
+const FS_DEFAULT_STACK_SIZE: usize = 8 * 1024 * 1024;
+
+/// Maximum number of threads used for running blocking operations (based on the tokio runtime
+/// default).
+///
+/// These threads are used mostly for blocking I/O operations, so they don't really
+/// consume CPU resources. That's why we can afford and should have a lot of them.
+const FS_DEFAULT_MAX_BLOCKING_THREADS: usize = 512;
 
 // ------------------------------------------------------------------------------------------------
 
@@ -262,4 +282,141 @@ async fn execute_all_phases(
     );
 
     Ok(get_exit_code_from_error_counter())
+}
+
+
+pub fn run_with_args(cli: Cli, token: CancellationToken) -> ExitCode {
+    let arg = from_main(&cli);
+
+    // Init tracing
+    let mut telemetry_handle = match init_tracing(FsTraceConfig::new_from_io_args(
+        arg.command,
+        cli.project_dir().as_ref(),
+        cli.target_path().as_ref(),
+        &arg.io,
+        "dbt-sa",
+    )) {
+        Ok(handle) => handle,
+        Err(e) => {
+            let msg = e.to_string();
+            print_trimmed_error(msg);
+            std::process::exit(1);
+        }
+    };
+
+    // XXX: when dbt-sa-cli and dbt-cli are unified, this will be the event emitter
+    // we inject into execute_fs. This instantiation is here as proof that our build
+    // and dependencies are configured such that private .proto files aren't linked
+    // into the SA code.
+    let _event_emitter = vortex_events::fusion_sa_event_emitter(false);
+
+    // Setup tokio runtime and set stack-size to 8MB
+    // DO NOT USE Rayon, it is not compatible with Tokio
+
+    let tokio_rt = match arg.num_threads {
+        Some(1) => {
+            // Simiulate single-threaded runtime
+            tokio::runtime::Builder::new_multi_thread()
+                .enable_all()
+                .thread_stack_size(FS_DEFAULT_STACK_SIZE)
+                .worker_threads(1)
+                .max_blocking_threads(1)
+                .build()
+                .expect("failed to initialize 'single-threaded' tokio runtime")
+        }
+        // Uncomment this if you want to limit the number of threads in multi-threaded runtime
+        // Some(num_threads) if num_threads > 1 => {
+        //     // Multi-threaded runtime: limit to num_threads
+        //     tokio::runtime::Builder::new_multi_thread()
+        //         .enable_all()
+        //         .worker_threads(num_threads)
+        //         .max_blocking_threads(FS_DEFAULT_MAX_BLOCKING_THREADS)
+        //         .thread_stack_size(FS_DEFAULT_STACK_SIZE)
+        //         .build()
+        //         .expect("failed to initialize multi-threaded tokio runtime")
+        // }
+        _ => {
+            // Multi-threaded runtime: use default (max parallelism)
+            tokio::runtime::Builder::new_multi_thread()
+                .enable_all()
+                .max_blocking_threads(FS_DEFAULT_MAX_BLOCKING_THREADS)
+                .thread_stack_size(FS_DEFAULT_STACK_SIZE)
+                .build()
+                .expect("failed to initialize default multi-threaded tokio runtime")
+        }
+    };
+
+    // If execution panics, exit with a status 2 (but not if RUST_BACKTRACE is
+    // set to 1, in which case we want to see the backtrace):
+    if std::env::var("RUST_BACKTRACE").unwrap_or_default() != "1" {
+        std::panic::set_hook(Box::new(|info| {
+            eprintln!("{} {}", RED.apply_to(PANIC), info);
+            let _ = io::stdout().flush();
+            let _ = io::stderr().flush();
+
+            std::process::exit(2);
+        }));
+    }
+
+    // Run within the process span
+    let future = Box::pin(execute_fs(arg, cli, token));
+
+    let result = tokio_rt.block_on(async { tokio_rt.spawn(future).await.unwrap() });
+
+    // Shut down telemetry
+    for err in telemetry_handle.shutdown() {
+        eprintln!("{}", err.pretty());
+    }
+
+    // Remove the panic hook
+    let _ = std::panic::take_hook();
+
+    // Handle regular execution
+    match result {
+        Ok(code) => {
+            // If exec succeeds, exit with status 0 or 1
+            // for 1 it is assumed that the  error was already printed)
+            assert!(code == 0 || code == 1);
+            ExitCode::from(code as u8)
+        }
+        Err(_err) => {
+            // If any step fails, assume error is already printed, just exit with a status 1
+            // show_progress_exit!(arg, start);
+            ExitCode::from(1)
+        }
+    }
+
+}
+
+pub fn print_trimmed_error(msg: String) {
+    let mut stderr = io::stderr();
+
+    let mut lines = msg.lines();
+    let mut command = String::new();
+
+    for line in lines.by_ref() {
+        if let Some(rest) = line.strip_prefix("error:") {
+            let _ = write!(stderr, "{}:", RED.apply_to("error"));
+            let _ = writeln!(stderr, "{rest}");
+        } else if let Some(rest) = line.trim_start().strip_prefix("tip:") {
+            let prefix = if line.starts_with("tip:") { "" } else { "  " };
+            let _ = write!(stderr, "{}{}", prefix, GREEN.apply_to("tip"));
+            let _ = writeln!(stderr, ":{rest}");
+        } else if line.trim().starts_with("Usage:") {
+            //let command = drop "Usage:"; take everything until the first '<'; trim
+            command = line.strip_prefix("Usage:").unwrap_or(line).to_string();
+            command = command
+                .split_once('<')
+                .unwrap_or(("", ""))
+                .0
+                .trim()
+                .to_string();
+            break; // stop before dumping giant usage block
+        } else {
+            let _ = writeln!(stderr, "{line}");
+        }
+    }
+
+    // Always print this footer
+    let _ = writeln!(stderr, "\nFor more information, try '{command} --help'.");
 }
